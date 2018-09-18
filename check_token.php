34538377e8cadcbee1be06b83c178717736ee0d3
SHEN <shen@SHENs-MacBook-Air.local>
2018-09-18 08:22:05 +0700

    just save token to Cookie

--- check_token.php
+++ check_token.php
@@ -18,16 +18,8 @@ if($logout) {
 $accessToken = $_COOKIE["access_token"];
 
 if(!$accessToken) {
-    $str_data = file_get_contents("token.json");
-    $data = json_decode($str_data,true);
-    $accessToken = $data["access_token"];
-    $expireAt = $data["expires_at"];
-    $isExpired = time() > $expireAt;
-}
-
-if(!$accessToken || $isExpired) {
     // get code param
-    $code = isset($_REQUEST['code']) ? $_REQUEST['code'] : null;
+    $code = isset($_GET['code']) ? $_GET['code'] : null;
     
     if(!$code) {
         header("Location: ".$GLOBALS['authLink']); 
@@ -43,28 +35,16 @@ if(!$accessToken || $isExpired) {
         $accessToken = $response['access_token'];
         $expire = $response['expires_in'];
         setcookie("access_token",$accessToken,time()+$expire);
-        $data["access_token"] = $accessToken;
-        $data["expires_at"] = time()+$expire;
-
+        
         $refreshToken = $response['refresh_token'];
         $refreshExpire = $response['refresh_expires_in'];
         setcookie("refresh_token",$refreshToken,time()+$refreshExpire);
-        $data["refresh_token"] = $refreshToken;
-        $data["refresh_expires_at"] = time()+$refreshExpire;
-
-        // write $data to FILE token.json
-        $fh = fopen("token.json", 'w')
-              or die("Error opening token file");
-        fwrite($fh, json_encode($data,JSON_UNESCAPED_UNICODE));
-        fclose($fh);
     } else {
         echo "GET TOKEN ERROR";
         //header("Location: ".$GLOBALS['authLink']); 
         exit();
     }
 } else {
-    // set token from FILE to COOKIE
-    setcookie("access_token",$accessToken,$expireAt);
 }
 
 ?>
