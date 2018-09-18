1e4af47a84b85562afaf3f78cf6029cf980e3b73
SHEN <shen@SHENs-MacBook-Air.local>
2018-09-18 08:19:57 +0700

    fix extra menu

--- extra.php
+++ extra.php
@@ -1,6 +1,5 @@
 <?php
 include_once "check_token.php";
-
 ?>
 
 <!DOCTYPE html>
@@ -8,26 +7,57 @@ include_once "check_token.php";
 <head>
   <title>EXTRA</title>
   <link rel="shortcut icon" type="image/x-icon" href="./ico/extra.ico" />
-</head>
 
+  	<script type="text/javascript">
+		function getCookie(cname) {
+		    var name = cname + "=";
+		    var decodedCookie = decodeURIComponent(document.cookie);
+		    var ca = decodedCookie.split(';');
+		    for(var i = 0; i <ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0) == ' ') {
+		            c = c.substring(1);
+		        }
+		        if (c.indexOf(name) == 0) {
+		            return c.substring(name.length, c.length);
+		        }
+		    }
+		    return "";
+		}
+		function eraseCookie(name) {   
+		    document.cookie = name+'=; Max-Age=-99999999;';  
+		}
+		function showToken(){
+			prompt("Copy token to clipboard: Ctrl+C, Enter", getCookie("access_token"));
+		}
+		function logout(){
+			deleteCookie("access_token");
+		}
+	</script>
+</head>
 <body>
+
 <div style ='font:30px/40px Arial,tahoma,sans-serif;'>
 
-<a target="_blank" href="/lazop/orders.php?needfull=1">Đơn hàng mới</href><br/>
-<a target="_blank" href="/lazop/products.php?status=sold-out">Danh sách sản phẩm</href><br/>
-<a target="_blank" href="/lazop/products1.php?status=sold-out">Danh sách sản phẩm (Paging)</href><br/>
+<a target="_blank" href="/lazop/orders.php?needfull=1">Đơn hàng mới</a><br/>
+<a target="_blank" href="/lazop/products.php?status=sold-out">Danh sách sản phẩm</a><br/>
+<a target="_blank" href="/lazop/products1.php?status=live">Danh sách sản phẩm (Paging)</a><br/>
 <hr> 
-<a target="_blank" href="/lazop/massclone.php">Mass clone SP</href><br/>
-<a target="_blank" href="/lazop/create.php">Tạo SP</href><br/>
-<a target="_blank" href="/lazop/addchild.php">Tạo SP con</href><br/>
-<a target="_blank" href="/lazop/update_gui.php">Cập nhật SP</href><br/>
-<a target="_blank" href="/lazop/massupdate.php">Mass update</href><br/>
+<a target="_blank" href="/lazop/massclone.php">Mass clone SP</a><br/>
+<a target="_blank" href="/lazop/massclone_shop2shop.php">Mass clone SHOP to SHOP</a><br/>
+<a target="_blank" href="/lazop/create.php">Tạo SP</a><br/>
+<a target="_blank" href="/lazop/addchild.php">Tạo SP con</a><br/>
+<a target="_blank" href="/lazop/update_gui.php">Cập nhật SP</a><br/>
+<a target="_blank" href="/lazop/massupdate.php">Mass update</a><br/>
 <hr> 
-<a target="_blank" href="/lazop/copyinfo.php">Copy thuộc tính SP</href><br/>
-<a target="_blank" href="/lazop/del.php">Xoá SP</href><br/>
+<a target="_blank" href="/lazop/copyinfo.php">Copy thuộc tính SP</a><br/>
+<a target="_blank" href="/lazop/del.php">Xoá SP</a><br/>
 <hr> 
-<a target="_blank" href="/lazop/previewimages.php">Preview images</href><br/>
-<a target="_blank" href="/lazop/previewproducts.php">Preview products</href><br/>
+<a target="_blank" href="/lazop/previewimages.php">Preview images</a><br/>
+<a target="_blank" href="/lazop/previewproducts.php">Preview products</a><br/>
+<hr>
+<a onclick="showToken()" href="#">Show token</a><br/>
+<a onclick="logout()" href="#">Logout</a><br/>
 </body>
 
 </html>
