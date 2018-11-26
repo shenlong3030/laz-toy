de7c4b1aa9f223a09726d9f7bafdd737b4245fa8
SHEN <shen@SHENs-MacBook-Air.local>
2018-11-26 22:25:22 +0700

    add logout button

--- index.php
+++ index.php
@@ -7,6 +7,32 @@ include_once "check_token.php";
 <head>
   <title>LAZADA Toys</title>
   <link rel="shortcut icon" type="image/x-icon" href="./ico/tool.ico" />
+  <script type="text/javascript">
+		function getCookie(cname) {
+		    var name = cname + "=";
+		    var decodedCookie = decodeURIComponent(document.cookie);
+		    var ca = decodedCookie.split(';');
+		    for(var i = 0; i <ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0) == ' ') {
+		            c = c.substring(1);
+		        }
+		        if (c.indexOf(name) == 0) {
+		            return c.substring(name.length, c.length);
+		        }
+		    }
+		    return "";
+		}
+		function deleteCookie( name ) {
+		  	document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+		}
+		function showToken(){
+			prompt("Copy token to clipboard: Ctrl+C, Enter", getCookie("access_token"));
+		}
+		function logout(){
+			deleteCookie("access_token");
+		}
+	</script>
 </head>
 
 <body>
@@ -17,6 +43,8 @@ include_once "check_token.php";
 <a target="_blank" href="/lazop/products1.php?status=all">Danh sách sản phẩm</href><br/>
 <hr>
 <a target="_blank" href="/lazop/>orders.php?needfull=1&shopid=01">Đơn hàng mới SHSHOP01</href><br/>
+<hr>
+<a onclick="logout()" href="#">Logout</a><br/>
 </body>
 
 </html>
