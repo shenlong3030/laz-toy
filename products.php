634163e3d121cd6511c656f84c0d8ca6b729b68b
macad <macad@MacBook-Air-cua-macad.local>
2020-11-15 23:28:20 +0700

    Product update qty, status with AJAX

--- products.php
+++ products.php
@@ -187,89 +187,87 @@ function search() {
 $(function(){
 
   //######### AJAX QUEUE ######################################################################################
-  var ajaxManager = (function() {
-     var requests = [];
+    var ajaxManager = (function() {
+       var requests = [];
 
-     return {
-        addReq:  function(opt) {
-            requests.push(opt);
-        },
-        removeReq:  function(opt) {
-            if( $.inArray(opt, requests) > -1 )
-                requests.splice($.inArray(opt, requests), 1);
-        },
-        run: function() {
-            var self = this,
-                oriSuc;
+       return {
+          addReq:  function(opt) {
+              requests.push(opt);
+          },
+          removeReq:  function(opt) {
+              if( $.inArray(opt, requests) > -1 )
+                  requests.splice($.inArray(opt, requests), 1);
+          },
+          run: function() {
+              var self = this,
+                  oriSuc;
 
-            if( requests.length ) {
-                oriSuc = requests[0].complete;
+              if( requests.length ) {
+                  oriSuc = requests[0].complete;
 
-                requests[0].complete = function() {
-                     if( typeof(oriSuc) === 'function' ) oriSuc();
-                     requests.shift();
-                     self.run.apply(self, []);
-                };   
+                  requests[0].complete = function() {
+                       if( typeof(oriSuc) === 'function' ) oriSuc();
+                       requests.shift();
+                       self.run.apply(self, []);
+                  };   
 
-                $.ajax(requests[0]);
-            } else {
-              self.tid = setTimeout(function() {
-                 self.run.apply(self, []);
-              }, 1000);
-            }
-        },
-        stop:  function() {
-            requests = [];
-            clearTimeout(this.tid);
-        }
-     };
-  }());
-
-  ajaxManager.run(); 
-
-  function myAjaxQueueUpdate(params) {
-      // send response to this iframe
-      var myFrame = $("#responseIframe").contents().find('body'); 
-
-      ajaxManager.addReq({
-           type: 'POST',
-           url: 'update-api.php',
-           data: params,
-           success: function(data){
-              var res = JSON.parse(data); // data is string, convert to obj
-              var d = new Date();
-              var n = d.toLocaleTimeString();
-
-              if(parseInt(res.code)) {
-                myFrame.prepend(n + data + '<br>'); 
+                  $.ajax(requests[0]);
               } else {
-                myFrame.prepend(n + ' SUCCESS<br>'); 
+                self.tid = setTimeout(function() {
+                   self.run.apply(self, []);
+                }, 1000);
               }
-           },
-           error: function(error){
-              myFrame.prepend(n + ' FAILED<br>'); 
-           }
-      });
-  }
-//##########################################################################################################
+          },
+          stop:  function() {
+              requests = [];
+              clearTimeout(this.tid);
+          }
+       };
+    }());
+    ajaxManager.run(); 
+
+    function productUpdateWithAjaxQueue(params) {
+        // send response to this iframe
+        var myFrame = $("#responseIframe").contents().find('body'); 
+
+        ajaxManager.addReq({
+             type: 'POST',
+             url: 'update-api.php',
+             data: params,
+             success: function(data){
+                var res = JSON.parse(data); // data is string, convert to obj
+                var d = new Date();
+                var n = d.toLocaleTimeString();
+
+                if(parseInt(res.code)) {
+                  myFrame.prepend(n + data + '<br>'); 
+                } else {
+                  myFrame.prepend(n + ' SUCCESS<br>'); 
+                }
+             },
+             error: function(error){
+                myFrame.prepend(n + ' FAILED<br>'); 
+             }
+        });
+    }
+  //##########################################################################################################
 
   $("button[name='qtyaction'][value='+500']").click(function() {
       $(this).parent().find('input[name=qty]').val('500'); 
       var sku = $(this).parent().find('input[name=sku]').val(); 
-      myAjaxQueueUpdate({ sku: sku, action: "qty", qty: 500});
+      productUpdateWithAjaxQueue({ sku: sku, action: "qty", qty: 500});
   });
   $("button[name='qtyaction'][value='=0']").click(function() {
       $(this).parent().find('input[name=qty]').val('0'); 
       var sku = $(this).parent().find('input[name=sku]').val(); 
-      myAjaxQueueUpdate({ sku: sku, action: "qty", qty: 0});
+      productUpdateWithAjaxQueue({ sku: sku, action: "qty", qty: 0});
   });
   $('input[name=qty]').keypress(function(event){
-    console.log("SHEN ENTER");
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){ // press ENTER
         var s = $(this).parent().find('input[name=sku]').val(); 
         var q = $(this).parent().find('input[name=qty]').val(); 
-        myAjaxQueueUpdate({ sku: s, action: "qty", qty: q});
+        productUpdateWithAjaxQueue({ sku: s, action: "qty", qty: q});
     }
     event.stopPropagation();
   });
