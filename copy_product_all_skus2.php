6ea043d3c5f2415ad6f39cafe69fe95918fe1bc0
shenlong3030 <shenlong3030@gmail.com>
2023-07-06 10:49:31 +0700

    fix copyCL2

--- copy_product_all_skus2.php
+++ copy_product_all_skus2.php
@@ -10,10 +10,11 @@ $action = isset($_REQUEST["action"]) ? $_REQUEST["action"] : "";
 $newName = val($_REQUEST["new_name"], "");
 $selectedVariations = val($_REQUEST["variations"], "");
 $newSkuPrefix = val($_REQUEST["new_sku_prefix"], "");
+$newSkuPrefix = trim($newSkuPrefix);
 $associatedSku = val($_REQUEST["associated_sku"], "");
 
-$models = val($_REQUEST['models'], null);
-$models = array_filter(explode("\n", str_replace("\r", "", $models)));
+$types = val($_REQUEST['type_screen_guards'], null);
+$types = array_filter(explode("\n", str_replace("\r", "", $types)));
 
 $productName;
 $srcSkuList;
@@ -36,7 +37,7 @@ if($sku) {
             $product = setProductAssociatedSku($product, $associatedSku);
             $product['Attributes']['name'] = $newName;
 
-            foreach($models as $j=>$model) {
+            foreach($types as $j=>$type) {
                 foreach ($productSkus as $i=>$item) {
                     $oldSku = $item['SellerSku']; 
                     // break if not selected for copying
@@ -44,19 +45,21 @@ if($sku) {
                         continue;
                     }
 
-                    $typeSG = $item['saleProp']['compatibility_by_model'];
-                    unset($item['saleProp']); // remove all other saleProp
-                    $item['saleProp']['type_screen_guard'] = $typeSG;
-                    $item['saleProp']['compatibility_by_model'] = $model;
+                    $item['saleProp']['type_screen_guard'] = $type;
+                    //$item['saleProp']['compatibility_by_model'] = $model;
 
                     //var_dump($item['saleProp']);
                     
                     // sku = "AAA__BBBBBBB__CCC.CC"  => postfix = CCC.CC
                     preg_match('/_([^_]+$)/', $item['SellerSku'], $match);
                     $postfix = count($match) ? $match[1] : "";
-                    $newSku = trim($newSkuPrefix) . $model . "." . trim($postfix);
-                    $newSku = strtoupper($newSku);
-                    $newSku = make_short_sku($newSku);
+
+                    if($newSkuPrefix[-1] != "_") {
+                        $newSkuPrefix .= "_";
+                    }
+
+                    $newSku = $newSkuPrefix . $type . "." . trim($postfix);
+                    $newSku = make_short_sku(strtoupper($newSku));
                     $item['SellerSku'] = $newSku;
                     $item['Status'] = "active";
 
@@ -69,13 +72,17 @@ if($sku) {
             $associatedProduct = getProduct($accessToken, $associatedSku);
             $newName = getProductName($associatedProduct);
 
-            preg_match('/(.+_)/', $associatedSku, $match);
-            $newSkuPrefix = count($match) ? $match[1] : "";
+            // get newSkuPrefix from associatedSku
+            // preg_match('/(.+_)/', $associatedSku, $match);
+            // $newSkuPrefix = count($match) ? $match[1] : "";
         }
 
-        preg_match('/(.+_)/', $sku, $match);
-        $skuPrefix = count($match) ? $match[1] : "";
-        $newSkuPrefix = $newSkuPrefix ? $newSkuPrefix : $skuPrefix;
+        //preg_match('/(.+_)/', $sku, $match);
+        //$skuPrefix = count($match) ? $match[1] : "";
+        //$newSkuPrefix = $newSkuPrefix ? $newSkuPrefix : $skuPrefix;
+
+        $newSkuPrefix = "CL2__";
+
         $productName = $newName ? $newName : $product['Attributes']['name'];
 
         $srcSkuList = array_map(function($productSkus){
@@ -141,10 +148,10 @@ $cloneLink = "https://$_SERVER[HTTP_HOST]/lazop/create.php?sku=$sku";
     NEW NAME: <input style="background: lightgreen" type="text" name="new_name" size="70" value="<?php echo $productName ?>"/><br/>
     NEW Associated Sku: <input style="background: lightgreen" type="text" name="associated_sku" size="70" value="<?php echo val($associatedSku, "") ?>"/><br/>
 <hr>
-    compatibility_by_model <br/>
-    <textarea class="nowrap" name="models" rows="6" cols="36"><?php echo implode("\n", $models);?></textarea>
+    type_screen_guard <br/>
+    <textarea class="nowrap" name="type_screen_guards" rows="6" cols="36"><?php echo implode("\n", $models);?></textarea>
 <hr>
-    select type_screen_guard <br/>
+    select compatibility_by_model <br/>
     <?php foreach($srcSkuList as $key=>$value):?>
         <input type="checkbox" name="variations[]" value="<?php echo $value;?>"/>
         <?php echo $variationList[$key]?>
